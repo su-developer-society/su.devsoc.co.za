032d990cc7772dda1e1659f52d8648cc6f70d2eb
Veer Gosai <71587878+VeerGosai@users.noreply.github.com>
2025-10-14 01:35:12 +0200

    Coming Soon

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // This file is kept for future functionality
+    // The countdown and form functionality have been removed
+    console.log("Stellenbosch University Developer Society - Coming Soon");
+});
