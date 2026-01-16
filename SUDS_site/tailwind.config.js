dfce37b99e2bc031abfd120faeaa582b7472632d
EllenKYG <29606926@sun.ac.za>
2026-01-17 01:03:57 +0200

    minor fix

--- SUDS_site/tailwind.config.js
+++ SUDS_site/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./app/**/*.{js,ts,jsx,tsx,mdx}'],
+  safelist: ['text-4xl', 'p-8', 'flex', 'items-center'], // temporary test
   theme: {
     extend: {},
   },
