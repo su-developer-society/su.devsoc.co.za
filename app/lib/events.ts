e49415dce5ea61a75f1087f032cec476762f85b7
Stephen Carter <162360946+stephennotsteven@users.noreply.github.com>
2026-02-13 23:16:45 +0200

    Added Div and Conquer Hackathon to Site

--- app/lib/events.ts
+++ app/lib/events.ts
@@ -18,4 +18,13 @@ export const events: Event[] = [
     link: "https://forms.cloud.microsoft/r/g8yESKdz92",
     image: "/events/welcoming-event-26.png",
   },
+  {
+    id: "div-and-conquer-hackathon-26",
+    title: "events.div-and-conquer-title",
+    description: "events.div-and-conquer-description",
+    startDate: "2026-02-27T17:30:00+02:00",
+    endDate: "2026-03-03T00:00:00+02:00",
+    link: "https://hackathon.devsoc.co.za",
+    image: "/events/div-and-conquer-2026.jpg",
+  },
 ];
