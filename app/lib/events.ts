558f8ad1da6866b47ddc9e9ff319aa0afefb061e
Stephen Carter <162360946+stephennotsteven@users.noreply.github.com>
2026-03-02 17:16:21 +0200

    Update events list by removing and adding events
    Removed the welcoming event and added a new Scytale deep dive event with details.

--- app/lib/events.ts
+++ app/lib/events.ts
@@ -10,21 +10,21 @@ export interface Event {
 
 export const events: Event[] = [
   {
-    id: "welcoming-event-26",
-    title: "events.welcoming-event-26-title",
-    description: "events.welcoming-event-26-description",
-    startDate: "2026-02-20T17:30:00+02:00",
-    endDate: "2026-02-20T21:30:00+02:00",
-    link: "https://forms.cloud.microsoft/r/g8yESKdz92",
-    image: "/events/welcoming-event-26.png",
-  },
-  {
     id: "div-and-conquer-hackathon-26",
     title: "events.div-and-conquer-title",
     description: "events.div-and-conquer-description",
     startDate: "2026-02-27T17:30:00+02:00",
-    endDate: "2026-03-03T00:00:00+02:00",
+    endDate: "2026-03-03T23:59:00+02:00",
     link: "https://hackathon.devsoc.co.za",
     image: "/events/div-and-conquer-2026.jpg",
+  },
+    {
+    id: "scytale-deep-dive-26",
+    title: "SU Developer Society x Scytale: Effects of AI on Security and Privacy Compliance ",
+    description: "Join us for an expert-driven lecture on the impact of AI on the GRC Industry. Also, enjoy Pizza, Merch and Networking Opportunities.",
+    startDate: "2026-03-05T17:30:00+02:00",
+    endDate: "2026-03-05T18:30:00+02:00",
+    link: "https://forms.cloud.microsoft/Pages/ResponsePage.aspx?id=Azv6pjwKWEKEM6Eg3_zTSPsybXK7K5JLha3az2Hhn3BURTJIUTQ5T05XUTVLWjg2WExSQTFVRUZUMi4u",
+    image: "/events/scytale-event.jpg",
   },
 ];
