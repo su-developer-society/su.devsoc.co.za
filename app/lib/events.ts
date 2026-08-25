e0214f7080f6e3ff4d7058ad6043cb88000702ee
Carter, SH, Mr [29063191@sun.ac.za] <29063191@sun.ac.za>
2026-08-25 23:07:23 +0200

    added election page

--- app/lib/events.ts
+++ app/lib/events.ts
@@ -10,30 +10,12 @@ export interface Event {
 
 export const events: Event[] = [
   {
-    id: "div-and-conquer-hackathon-26",
-    title: "events.div-and-conquer-title",
-    description: "events.div-and-conquer-description",
-    startDate: "2026-02-27T17:30:00+02:00",
-    endDate: "2026-03-03T23:59:00+02:00",
-    link: "https://hackathon.devsoc.co.za",
-    image: "/events/div-and-conquer-2026.jpg",
-  },
-    {
-    id: "scytale-deep-dive-26",
-    title: "SU Developer Society x Scytale: Effects of AI on Security and Privacy Compliance ",
-    description: "Join us for an expert-driven lecture on the impact of AI on the GRC Industry. Also, enjoy Pizza, Merch and Networking Opportunities.",
-    startDate: "2026-03-05T17:30:00+02:00",
-    endDate: "2026-03-05T18:30:00+02:00",
-    link: "https://forms.cloud.microsoft/Pages/ResponsePage.aspx?id=Azv6pjwKWEKEM6Eg3_zTSPsybXK7K5JLha3az2Hhn3BURTJIUTQ5T05XUTVLWjg2WExSQTFVRUZUMi4u",
-    image: "/events/scytale-event.png",
-  },
-  {
-    id: "what-is-plone-26",
-    title: "What the @#$ is Plone? with Juizi",
-    description: "Get in-depth insights into one of the world’s most succesful open-sources projects from the professionals who use it on the daily. Discover how contributing to open source can supercharge your skills, strengthen your CV, and open doors to exciting career opportunities. Learn practical Git and collaboration best practices, and hear real-world insights into building and running a web development business in South Africa. Whether you're contributing to your first project or dreaming of your own tech venture, this session is for you.",
-    startDate: "2026-07-29T17:30:00+02:00",
-    endDate: "2026-07-29T18:30:00+02:00",
-    link: "https://tally.so/r/PdWPQP",
-    image: "/events/what-is-plone-26.png",
+    id: "exco_election_agm_26",
+    title: "Executive Committee Election & AGM 2026",
+    description: "Join us for the Executive Committee Election and Annual General Meeting (AGM) of the Developer Society. This is your opportunity to shape the future of our society, participate in leadership, and contribute to our community's growth. All members are encouraged to attend, vote, and engage in the decision-making process.",
+    startDate: "2026-09-03T17:30:00+02:00",
+    endDate: "2026-09-03T20:00:00+02:00",
+    link: "/election",
+    image: "/events/SU_devsoc_exco_election_agm.png",
   },
 ];
